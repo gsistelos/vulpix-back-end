feaa160663e9ebd7a1e78a3c1ad88814755cf224
gsistelos <gsistelos@proton.me>
2025-01-15 00:26:53 +0000

    fix: jest imports

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,11 +1,16 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule, UsersModule],
+      providers: [AuthService],
       controllers: [AuthController],
     }).compile();
 

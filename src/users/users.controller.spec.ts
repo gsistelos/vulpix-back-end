48d40897c6b8541bd1cbeea4781ebf9f76ba639a
gsistelos <gsistelos@proton.me>
2025-01-15 13:21:59 +0000

    feat: user create test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,22 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { hashPassword } from 'src/lib/hash';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockService = {
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [PrismaModule],
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should create a user with hashed password', async () => {
+      const createUserDto: CreateUserDto = {
+        username: 'John Doe',
+        email: 'john.doe@example.com',
+        password: 'password123',
+      };
+
+      mockService.create.mockImplementation((dto) => dto);
+
+      const result = await controller.create({ ...createUserDto });
+
+      expect(result).toEqual({
+        ...createUserDto,
+        password: hashPassword(createUserDto.password),
+      });
+    });
   });
 });

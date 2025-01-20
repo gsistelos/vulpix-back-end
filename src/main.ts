607758b9a7ffcdeaa27c740bf34077c873b2bdb2
gsistelos <gsistelos@proton.me>
2025-01-20 15:20:44 +0000

    base swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
@@ -11,6 +12,17 @@ async function bootstrap() {
     credentials: true,
   });
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('vulpix-back-end')
+    .setDescription('API Documentation for vulpix-back-end')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

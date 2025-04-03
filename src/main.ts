42a18404abf24de7a8ac8071380643cf6752fc04
gsistelos <gsistelos@proton.me>
2025-04-03 20:59:36 -0300

    feat: https

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,25 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: readFileSync('./secrets/private-key.pem'),
+    cert: readFileSync('./secrets/public-certificate.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   });
+
   app.use(cookieParser());
 
   const config = new DocumentBuilder()
@@ -25,4 +36,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 8080);
 }
+
 bootstrap();
